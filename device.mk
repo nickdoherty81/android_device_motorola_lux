d904860494a250ded9926768a08cb47628af55da
Josh Chasky <jchasky@gmail.com>
2016-02-27 01:36:39 -0700

    Lux: Enable MTP/ADB by default
    Change-Id: I01f7245b1b5128e2646c290f53a0d32df85e1495

--- device.mk
+++ device.mk
@@ -55,3 +55,9 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.target.rc
+
+# set default USB configuration
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    sys.usb.config=mtp,adb \
+    persist.sys.usb.config=mtp,adb \
+    ro.adb.secure=0
