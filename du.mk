cd006ac67b57c3bfc2d1677c85b70859cf4f481b
Alex Cruz <mazdarider23@gmail.com>
2016-12-15 17:56:39 +0100

    Remove nfc_enhanced.mk
    - Remove unused overlay. We don't have this commit nor do we plan on including it
    https://github.com/CyanogenMod/android_packages_apps_Settings/commit/e64341f92d7a268fd770bb9421a87418a04b50e8
    
    Change-Id: I8b321adfd931f297355df756895e338721af8969

--- du.mk
+++ du.mk
@@ -14,10 +14,6 @@
 
 $(call inherit-product, device/motorola/lux/full_lux.mk)
 
-# Boot animation
-TARGET_SCREEN_WIDTH := 1080
-TARGET_SCREEN_HEIGHT := 1920
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := du_lux
 PRODUCT_DEVICE := lux
