8ee93ea427a65ac7e1e36591943db326598363df
nickd81 <nickdoh81@gmail.com>
2017-01-14 00:57:24 -0500

    Sharing is caring
    Change-Id: Iee01c4d615f93fc4ced40496bf089de10cc0a4ab

--- device.mk
+++ device.mk
@@ -19,10 +19,6 @@ $(call inherit-product, device/motorola/msm8916-common/msm8916.mk)
 # Screen density
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
-# Dalvik
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
-
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
